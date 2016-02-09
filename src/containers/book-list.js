53bf60d6c58d6923ec3db1292a59d83110a05568
Juan <jguardado85@gmail.com>
2016-02-09 14:27:02 -0800

    first commit

---
+++ src/containers/book-list.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
+
+class BookList extends Component {
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <li key={book.title} className="list-group=item">{book.title}</li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  //whatever gets returned from here shows up as props in booklist
+  return {
+    books: state.books,
+  };
+}
+
+//Anything returned from this function will end up as props on container
+function mapDispatchToProps(dispatch) {
+  //whenever selectbook is called teh result should be passed to all of our reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+//Promote Booklist from a Componenet to a Container
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: the Good Parts' },
+    { title: 'Harry Poter' },
+    { title: 'The Dark Tower' },
+    { title: 'Eloquent Ruby' },
+  ];
+}
